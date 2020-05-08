c6c6046d242f3098efc281ceb8fd5a741f2d7ba1
Stephanie Podolak <StephPodolak@gmail.com>
2020-05-08 08:49:30 -0700

    create CurrentBloodSugar.js component to display blood sugar after logging into dexcom

---
+++ src/CurrentBloodSugar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function CurrentBloodSugar() {
+  return ( 
+  <React.Fragment>     
+    <p>Current Blood Sugar: 136</p>
+  </React.Fragment>
+  )}
+
+export default CurrentBloodSugar;
