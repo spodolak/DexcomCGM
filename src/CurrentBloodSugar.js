003f6cd380b8a514dc94f9262fd5d97bb86f3a9e
Stephanie Podolak <StephPodolak@gmail.com>
2020-05-08 13:20:34 -0700

    add logic to CurrentBloodSugar.js to return bearer token when authorization code is hard coded. bearer token appears in console.

--- src/CurrentBloodSugar.js
+++ src/CurrentBloodSugar.js
@@ -1,10 +1,38 @@
 import React from 'react';
 
-function CurrentBloodSugar() {
+var data = `client_secret=${process.env.REACT_APP_CLIENT_SECRET}&client_id=${process.env.REACT_APP_CLIENT_ID}&code=${process.env.REACT_APP_CODE}&grant_type=authorization_code&redirect_uri=http://localhost:3000`;
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("POST", "https://api.dexcom.com/v2/oauth2/token");
+xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+xhr.setRequestHeader("cache-control", "no-cache");
+
+xhr.send(data);
+
+class CurrentBloodSugar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      currentBloodSugar: [134]
+    };
+  }
+
+  render() {
   return ( 
   <React.Fragment>     
-    <p>Current Blood Sugar: 136</p>
+    <p>Current Blood Sugar: {this.state.currentBloodSugar}</p>
   </React.Fragment>
   )}
+}
 
 export default CurrentBloodSugar;
