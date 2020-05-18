fec1360415d389f77bd81826ae260eb342cf8704
Stephanie Podolak <StephPodolak@gmail.com>
2020-05-18 11:38:51 -0700

    stylize landing welcome page

--- src/Dexcom.js
+++ src/Dexcom.js
@@ -6,15 +6,23 @@ const clientId = process.env.REACT_APP_CLIENT_ID;
 function Dexcom(props) {
   return ( 
   <React.Fragment>     
-    <a
-      className="App-link"
-      href={'https://api.dexcom.com/v2/oauth2/login?client_id='+ clientId + '&redirect_uri=http://localhost:3000&response_type=code&scope=offline_access&state=auth'}
-      target="_blank"
-      rel="noopener noreferrer"
-      onClick={()=> props.onClickingLogIn}
-    >
-      Log In
-    </a>
+    <div class="welcome-page">
+      <h1>Welcome!</h1>
+      <a
+        class="App-link"
+        href={'https://api.dexcom.com/v2/oauth2/login?client_id='+ clientId + '&redirect_uri=http://localhost:3000&response_type=code&scope=offline_access&state=auth'}
+        target="_blank"
+        rel="noopener noreferrer"
+        onClick={()=> props.onClickingLogIn}
+      >
+      {/* <button class="btn button-outline button-login">
+        Log In
+      </button> */}
+      <button class="outline">
+        Log In
+      </button>
+      </a>
+    </div>
   </React.Fragment>
   )}
 
