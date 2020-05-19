e1dad7eb3f06f90559f0b7b7af498df9e8e19695
Stephanie Podolak <StephPodolak@gmail.com>
2020-05-19 13:36:28 -0700

    add form to calibrate current blood sugar. styled with react-bootstrap

--- src/components/Calibrate.js
+++ src/components/Calibrate.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import drop from './../img/drop.svg'
+import { Form, Row, Col, InputGroup } from 'react-bootstrap';
 
 function Drop(props) {
   return ( 
-  <React.Fragment>     
-    <div class="drop">
-      <img src={drop} width = "60px" alt="icon"/>
-    </div>
-    <div>
-      <button class="outline" onClick={()=> props.onSwitchingViews('')}>
-        Done
-      </button>
-    </div>
+      <React.Fragment>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label><h1>Enter Current Blood Sugar</h1></Form.Label>
+            <InputGroup size="lg" >
+              <Form.Control classType="text-center" type="calibrate" placeholder="mg/dL" />
+            </InputGroup>
+            <Form.Text className="text-muted">
+              1. Wash and dry your hands <br></br>
+              2. Take a fingerstick with your meter<br></br>
+              3. Promptly enter the exact value from your meter<br></br>
+            </Form.Text>
+          </Form.Group>
+          <button class="outline" onClick={()=> props.onSwitchingViews('')} type="submit">
+            Calibrate
+          </button>
+        </Form>     
   </React.Fragment>
   )}
 
