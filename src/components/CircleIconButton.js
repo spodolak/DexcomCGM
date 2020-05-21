b6097bb0108fb9f9320f640d31d9050979e208f1
Stephanie Podolak <StephPodolak@gmail.com>
2020-05-20 20:58:09 -0700

    format react-bootstrap Cols

--- src/components/CircleIconButton.js
+++ src/components/CircleIconButton.js
@@ -9,22 +9,22 @@ import "bootstrap/dist/css/bootstrap.css";
 function CircleIconButton(props) {
   return ( 
   <React.Fragment> 
+      <Row className="mt-5">
+      </Row>
       <Row>
         <Col className="justify-content-center mt-5">
+          <button class="round" onClick={()=> props.onSwitchingViews('calibrate')}>
+            <img src={drop} width = "80px" alt="icon"/>
+          </button>
+        </Col >
+        <Col className="justify-content-center mt-3 mr-2">
           <button class="round" onClick={()=> props.onSwitchingViews('add_symptom')}>
             <img src={icon} width = "100px" alt="icon"/>
           </button>
         </Col>
-      </Row>
-      <Row>
-        <Col className="justify-content-left mt-4 mr-2">
-          <button class="round" onClick={()=> props.onSwitchingViews('calibrate')}>
-            <img src={drop} width = "70px" alt="icon"/>
-          </button>
-        </Col >
-        <Col className="justify-content-right mt-4 ml-2">
+        <Col xs="5"className="justify-content-center mt-5">
           <button class="round" onClick={()=> props.onSwitchingViews('graph')}>
-            <img src={graph} width = "70px" alt="icon"/>
+            <img src={graph} width = "80px" alt="icon"/>
           </button>
         </Col>
       </Row>
