79508b117d119e1bb0cea74bf2718483487a53ad
Stephanie Podolak <StephPodolak@gmail.com>
2020-05-25 21:27:17 -0700

    add logic to change orb display based on current glucose value

--- src/components/Orb.js
+++ src/components/Orb.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-function Orb() {
-  return ( 
-  <React.Fragment>     
-    <div className="orb"></div>
-  </React.Fragment>
-  )
+function Orb(props) {
+  if ( props.egv < 70) {
+    return ( 
+      <React.Fragment>     
+        <div className="orb-low"></div>
+      </React.Fragment>
+    )
+  } else if ( props.egv > 200 ) {
+    return ( 
+      <React.Fragment>     
+        <div className="orb-high"></div>
+      </React.Fragment>
+    )
+  } else {
+    return ( 
+      <React.Fragment>     
+        <div className="orb-normal"></div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default Orb;
