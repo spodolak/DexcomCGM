1af1c66e13104f0ccce0790a33be8349a6eb1782
Stephanie Podolak <StephPodolak@gmail.com>
2020-05-25 17:04:44 -0700

    add DexcomControl.js to components directory

---
+++ src/components/DexcomControl.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import AppControl from './AppControl';
+import Dexcom from './Dexcom';
+
+class DexcomControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dexcomLoggedIn: false
+    };
+  }
+
+  handleDexcomLogin = () => {
+    this.setState({dexcomLoggedIn: true});
+  }
+
+  render(){
+    let currentlyVisibleState = null;
+
+    if (window.location.search !== "") {     
+      currentlyVisibleState = <AppControl />
+    } else if (window.location.search === "") {
+      let authorizationCode = window.location.search;
+      currentlyVisibleState = 
+      <Dexcom onClickingLogIn = {this.handleDexcomLogin} code = { authorizationCode } />
+    } 
+    
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+      </React.Fragment>
+    );
+  }
+}
+
+export default DexcomControl;
