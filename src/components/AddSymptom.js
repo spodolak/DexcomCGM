ba5077d122826fa7a07ab63b8a668c8635d4c83d
Stephanie Podolak <StephPodolak@gmail.com>
2020-05-27 10:51:41 -0700

    add header text

--- src/components/AddSymptom.js
+++ src/components/AddSymptom.js
@@ -5,34 +5,36 @@ import { Form, Row } from 'react-bootstrap';
 function AddSymptom(props) {
   return ( 
   <React.Fragment>
+    <h1>How do you feel?</h1>
+    <br></br>
     <Form>
       {['checkbox'].map((type) => (
         <div key={`inline-${type}`} className="mb-3 ml-4">
           <Row className="justify-content-center">
             <div className="cc-selector">
-              <input id="sweaty" type="checkbox" name="credit-card" value="sweaty" />
+              <input id="sweaty" type="checkbox" value="sweaty" />
               <label className="drinkcard-cc sweaty" for="sweaty"></label>
             </div>
             <div className="cc-selector">
-              <input id="weak" type="checkbox" name="credit-card" value="weak" />
+              <input id="weak" type="checkbox" value="weak" />
               <label className="drinkcard-cc weak" for="weak"></label>
             </div>
             <div className="cc-selector">
-              <input id="irritable" type="checkbox" name="credit-card" value="irritable" />
+              <input id="irritable" type="checkbox" value="irritable" />
               <label className="drinkcard-cc irritable" for="irritable"></label>
             </div>
           </Row>
           <Row className="justify-content-center">
             <div className="cc-selector">
-              <input id="hungry" type="checkbox" name="credit-card" value="hungry" />
+              <input id="hungry" type="checkbox" value="hungry" />
               <label className="drinkcard-cc hungry" for="hungry"></label>
             </div>
             <div className="cc-selector">
-              <input id="dizzy" type="checkbox" name="credit-card" value="dizzy" />
+              <input id="dizzy" type="checkbox" value="dizzy" />
               <label className="drinkcard-cc dizzy" for="dizzy"></label>
             </div>
             <div className="cc-selector">
-              <input id="sleepy" type="checkbox" name="credit-card" value="sleepy" />
+              <input id="sleepy" type="checkbox" value="sleepy" />
               <label className="drinkcard-cc sleepy" for="sleepy"></label>
             </div>
           </Row>
