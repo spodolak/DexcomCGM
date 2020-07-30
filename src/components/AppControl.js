49cefab70eb7ca811a6475394deb43989ca33bf4
Stephanie Podolak <StephPodolak@gmail.com>
2020-07-29 19:35:44 -0700

    add timer to fetch bloodsugar value  every 1 min

--- src/components/AppControl.js
+++ src/components/AppControl.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import CurrentBloodSugar from './CurrentBloodSugar';
 import Calibrate from './Calibrate';
-import AddSymptom from './AddSymptom';
 import AddHighSymptom from './AddHighSymptom';
+import AddLowSymptom from './AddLowSymptom';
+import AddSymptom from './AddSymptom';
 import DexcomError from './DexcomError';
 import Graph from './Graph';
 import FootNavigation from './AppFooter.js';
@@ -14,9 +15,12 @@ class AppControl extends React.Component {
             error: null,
             isLoaded: false,
             bearerToken: '',
-            currentBloodSugar: 140,
+            currentBloodSugar: null,
+            lowAlert: 70,
+            highAlert: 200,
             bloodSugarValues: null,
-            currentView: null
+            currentView: null,
+            timer: null
         };
     }
 
@@ -49,8 +53,24 @@ class AppControl extends React.Component {
             .catch(error => console.log('error', error));
     }
 
+    getTime = () => {
+        function addZero(n) {
+            if (n < 10) {
+                return "0" + n;
+            }
+            return n.toString();
+        }
+        
+        let time = new Date();
+        let hours = addZero(time.getHours());
+        let minutes = addZero(time.getMinutes());
+        let seconds = addZero(time.getSeconds());
+        
+        return hours + ":" + minutes + ":" + seconds;
+    }    
     getBloodSugars = (token) => {
-
+        let currentTime = this.getTime(); 
+        
         var myHeaders = new Headers();
         myHeaders.append("Authorization", `Bearer ${token}`);
 
@@ -59,7 +79,7 @@ class AppControl extends React.Component {
             headers: myHeaders,
             redirect: 'follow'
         };
-        fetch("https://sandbox-api.dexcom.com/v2/users/self/egvs?startDate=2020-05-26T00:00:00&endDate=2020-05-26T23:59:00", requestOptions) //Sandbox data URL
+        fetch(`https://sandbox-api.dexcom.com/v2/users/self/egvs?startDate=2020-05-26T00:00:00&endDate=2020-05-26T${currentTime}`, requestOptions) //Sandbox data URL
             // fetch("https://api.dexcom.com/v2/users/self/egvs?startDate=2020-05-26T00:00:00&endDate=2020-05-26T23:59:00", requestOptions) //Dexcom user URL
             .then(response => response.json())
             .then((response) => {
@@ -69,12 +89,27 @@ class AppControl extends React.Component {
             .catch(error => console.log('error', error));
     }
 
+    
+    
+    
     componentDidMount() {
-        this.props.onLogIn()
         if (this.state.bearerToken === '') {
             this.getBearerToken();
         } else {
             this.getBloodSugars(this.state.bearerToken);
+        }
+        setInterval(() => {
+            console.log("blood sugar update", this.state.currentBloodSugar)
+            this.getBloodSugars(this.state.bearerToken);
+        }, 60000);
+    }
+    
+    handleAlert = () => {
+        if (this.state.currentBloodSugar < this.state.lowAlert ) {
+            this.setState({currentView: 'add_low_symptom'})
+        }
+        if (this.state.currentBloodSugar > this.state.highAlert ) {
+            this.setState({currentView: 'add_high_symptom'})
         }
     }
 
@@ -91,22 +126,25 @@ class AppControl extends React.Component {
         if (this.state.bloodSugarValues != null) {
             switch (this.state.currentView) {
                 case 'calibrate':
-                    currentlyVisibleState = <Calibrate onCalibrate={this.handleCalibrate} onSwitchingViews={this.handleSwitchingViews} />
+                    currentlyVisibleState = <Calibrate onCalibrate={this.handleCalibrate} onSwitchingViews={this.handleSwitchingViews} alertCheck = {this.handleAlert}/>
                     break;
-                // case 'add_symptom':
-                //     currentlyVisibleState = <AddSymptom onSwitchingViews={this.handleSwitchingViews} />
-                //     break;
                 case 'add_symptom':
+                    currentlyVisibleState = <AddSymptom onSwitchingViews={this.handleSwitchingViews} />
+                    break;
+                case 'add_low_symptom':
+                    currentlyVisibleState = <AddLowSymptom onSwitchingViews={this.handleSwitchingViews} />
+                    break;
+                case 'add_high_symptom':
                     currentlyVisibleState = <AddHighSymptom onSwitchingViews={this.handleSwitchingViews} />
                     break;
                 case 'graph':
                     currentlyVisibleState = <Graph values={this.state.bloodSugarValues} onSwitchingViews={this.handleSwitchingViews} />
                     break;
                 case 'home':
-                    currentlyVisibleState = <CurrentBloodSugar value={this.state.currentBloodSugar} onSwitchingViews={this.handleSwitchingViews} />
+                    currentlyVisibleState = <CurrentBloodSugar value={this.state.currentBloodSugar} lowLimit={this.state.lowAlert} highLimit={this.state.highAlert} onSwitchingViews={this.handleSwitchingViews} />
                     break;
                 default:
-                    currentlyVisibleState = <CurrentBloodSugar value={this.state.currentBloodSugar} onSwitchingViews={this.handleSwitchingViews} />
+                    currentlyVisibleState = <CurrentBloodSugar value={this.state.currentBloodSugar} lowLimit={this.state.lowAlert} highLimit={this.state.highAlert} onSwitchingViews={this.handleSwitchingViews} />
             }
         } else {
             currentlyVisibleState = <DexcomError />
